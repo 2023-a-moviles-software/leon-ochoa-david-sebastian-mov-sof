b4183769ce51d3603a77e2c35a226ee586d9a3dc
David León <david.leon@epn.edu.ec>
2023-07-11 19:58:07 -0500

    New Upload

--- 01-Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
@@ -51,6 +51,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Base de Datos sqlite
+        // MAIN ACTIVITY
+        EBaseDeDatos.tablaEntrenador = ESqliteHelperEntrenador(this)
+
         val botonCicloVida = findViewById<Button>(
             R.id.btn_ciclo_vida
         )
@@ -78,6 +82,18 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener {
                 abrirActividadConParametros(CIntentExplicitoParametros::class.java)
             }
+
+        val botonSqlite = findViewById<Button>(R.id.btn_sqlite)
+        botonSqlite
+            .setOnClickListener {
+                irActividad(ECrudEntrenador::class.java)
+            }
+
+        val botonRView = findViewById<Button>(R.id.btn_revcycler_view)
+        botonRView
+            .setOnClickListener {
+                irActividad(FRecyclerView::class.java)
+            }
     }
 
 
@@ -104,5 +120,4 @@ class MainActivity : AppCompatActivity() {
         callbackContenidoIntentExplicito
             .launch(intentExplicito)
     }
-
 }
